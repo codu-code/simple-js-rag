de908133812ee86cd8b4d03657c95cbc6156697c
NiallJoeMaher <nialljoemaher@gmail.com>
2024-05-29 10:55:49 +0100

    Update RAG route to use routerQueryEngine

--- src/app/api/rag/route.ts
+++ src/app/api/rag/route.ts
@@ -1,37 +1,65 @@
-import { VectorStoreIndex, OpenAI, Settings } from "llamaindex";
-import { LlamaParseReader } from "llamaindex/readers/LlamaParseReader";
+import {
+  VectorStoreIndex,
+  OpenAI,
+  Settings,
+  RouterQueryEngine,
+} from "llamaindex";
+import { TextFileReader } from "llamaindex/readers/TextFileReader";
 
 Settings.llm = new OpenAI({ model: "gpt-3.5-turbo" });
 
 export async function POST(request: Request) {
   try {
     const { query } = await request.json();
+
     if (!query) {
       throw new Error("Input is required");
     }
 
-    const reader = new LlamaParseReader({ resultType: "markdown" });
-
-    const documents = await reader.loadData(
-      "./src/data/writing-effectively.pdf"
+    // Get data from multiple files
+    const irelandDoc = await new TextFileReader().loadData(
+      "./src/data/ireland.txt"
+    );
+    const irishLangDoc = await new TextFileReader().loadData(
+      "./src/data/irish-language.txt"
+    );
+    const irishMythDoc = await new TextFileReader().loadData(
+      "./src/data/irish-mythology.txt"
     );
 
-    // Split text and create embeddings. Store them in a VectorStoreIndex
-    const index = await VectorStoreIndex.fromDocuments(documents);
+    // Create seperate VectorStoreIndex from documents
+    const irelandIndex = await VectorStoreIndex.fromDocuments(irelandDoc);
+    const irishLangIndex = await VectorStoreIndex.fromDocuments(irishLangDoc);
+    const irishMythIndex = await VectorStoreIndex.fromDocuments(irishMythDoc);
 
     // Query engine convenience function
-    // This convenience function combines several components:
-    // - Retriever
-    // - Postprocessing
-    // - Synthesizer
-    const queryEngine = index.asQueryEngine();
-    const { response } = await queryEngine.query({
-      query,
+    const irelandQueryEngine = irelandIndex.asQueryEngine();
+    const irishLangQueryEngine = irishLangIndex.asQueryEngine();
+    const irishMythQueryEngine = irishMythIndex.asQueryEngine();
+
+    const routerQueryEngine = await RouterQueryEngine.fromDefaults({
+      queryEngineTools: [
+        {
+          queryEngine: irelandQueryEngine,
+          description: "Information realted to the country of Ireland",
+        },
+        {
+          queryEngine: irishLangQueryEngine,
+          description: "Information about the Irish language",
+        },
+        {
+          queryEngine: irishMythQueryEngine,
+          description:
+            "Information about Irish mythology and folklore or stories",
+        },
+      ],
     });
 
+    const { response } = await routerQueryEngine.query({ query });
+
     return Response.json({ response });
-  } catch (err) {
-    console.log(err);
+  } catch (e) {
+    console.log(e);
     return new Response(`Something went wrong.`, { status: 400 });
   }
 }
